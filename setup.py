f239ab3f07614c1eda803c10a9ea053e60966221
Frank Sachsenheim <funkyfuture@riseup.net>
2017-04-14 14:19:29 +0200

    Adds a setup.py
    also changes README to .rst for the PyPI

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='rasmipy',
+    version='0.1rc1',
+    python_requires='>=3',
+    packages=['rasmipy'],
+    url='https://github.com/telota/rasmipy',
+    license='LGPLv3',
+    author='Oliver Pohl, Frank Sachsenheim',
+    author_email='telota@bbaw.de',
+    description='Reduce Arabic strings to their rasm, i.e. remove vocalization and other diacritics.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: Arabic',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Linguistic'
+    ]
+)
